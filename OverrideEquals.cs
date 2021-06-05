297de23eb4136ed7170909cb7292eb4c6d6cc808
mallikharjuna.mannam <m.malli0005@gmail.com>
2021-06-05 14:36:39 +0530

    Added String and Equal method ovveriding in child class 2.single and multi cast delegate

---
+++ OverrideEquals.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeInterviewQuestions
+{
+    class OverrideEquals
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 10;
+            int j = 10;
+            //we can convert general datatype from int to string using boxing and unboxing 
+            //but we cann't convert custom datatype 
+            //so we need to ovverride the tostring method to print the custom type object data
+            Console.WriteLine(i.ToString());
+
+            //using equal operator and equal method to comparing 2 general data type data but we can't get exact output using              
+            //equal operator and equal method so we must ovveride equla method and check obj!=null,the second both the object type should be same type then we can compare the two objects data
+
+            Console.WriteLine(i==j);
+            Console.WriteLine(i.Equals(j));
+
+            CustomerEqual c1 = new CustomerEqual();
+            c1.FirstName = "mallikarjuna";
+            c1.SecondName = "mallikarjuna";
+            CustomerEqual c2 = c1;
+            //print the custom data type data using ovveride tostring()
+
+            Console.WriteLine(c1.ToString());
+
+            Console.WriteLine("==================");
+            Console.WriteLine(c1 == c2);  //result =true becz both the object reference variable pointing to the same object so reference equality and value equality true
+            Console.WriteLine(c1.Equals(c2)); // reference equality and value equality are same 
+            //Since  c1 and c2 both refer to the same object the reference equality and value equality is true
+            Console.WriteLine("========================");
+
+            CustomerEqual c3 = new CustomerEqual();
+            c3.FirstName = "mallikarjuna";
+            c3.SecondName = "mallikarjuna";
+
+            Console.WriteLine(c1 == c3);//return false because c1 and c3 referring to different objects
+            Console.WriteLine(c1.Equals(c3));//retuen false inspite of the values across c1 and c2 beign same hence make secnce to override the Equal() method to return true
+
+            CustomerEqual c4 = new CustomerEqual();
+            c4.FirstName = "mallikarjuna";
+            c4.SecondName = "mallikha";
+
+           
+            Console.WriteLine("=========================");
+            Console.WriteLine(c1.Equals(c4));
+
+
+            Console.ReadLine();
+        }
+    }
+    public class CustomerEqual
+    {
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public override string ToString()
+        {
+            return this.FirstName + "," + this.SecondName;
+        }
+        public override bool Equals(object obj)
+        {
+
+            //make sure the passed in object is not null and can be casted to the type we are comparing
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is CustomerEqual))
+            {
+                return false;
+            }
+
+            return this.FirstName == ((CustomerEqual)obj).FirstName && this.SecondName == ((CustomerEqual)obj).SecondName;
+        }
+        public override int GetHashCode()
+        {
+            return FirstName.GetHashCode() ^ SecondName.GetHashCode(); 
+        }
+    }
+}
--- SingleCastDelegate.cs
+++ SingleCastDelegate.cs
@@ -8,6 +8,7 @@ namespace PracticeInterviewQuestions
 {
     public delegate int DelAddTwoValues(int i, int j);
     public delegate string DelSayHello(string name);
+    public delegate void DelGetArea(double width, double height);
     class SingleCastDelegate
     {
         public int AddTwoValues(int a,int b)
@@ -19,10 +20,28 @@ namespace PracticeInterviewQuestions
         {
             return "Hello " + name;
         }
+
+        public void GetArea(double width,double height)
+        {
+            Console.WriteLine(width*height);
+        }
+
+        public void GetPerimeter(double width, double height)
+        {
+            Console.WriteLine((2*(width * height)));
+        }
         static void Main(string[] args)
         {
 
             SingleCastDelegate singleCastDelegate = new SingleCastDelegate();
+            //singleCastDelegate.GetArea(100, 200);
+            //singleCastDelegate.GetPerimeter(300, 400);
+            //create the object to MulticastDelegate 
+            //DelGetArea multiCastdDlGetArea = new DelGetArea(singleCastDelegate.GetArea);
+            //multiCastdDlGetArea.Invoke(200, 400);
+            DelGetArea multiCastdDlGetArea = singleCastDelegate.GetArea;
+            multiCastdDlGetArea += singleCastDelegate.GetPerimeter;
+            multiCastdDlGetArea.Invoke(200, 400);
 
             //create the object for delegate beacause of delegate is args reference type
             DelAddTwoValues delAddTwoValues = new DelAddTwoValues(singleCastDelegate.AddTwoValues);
