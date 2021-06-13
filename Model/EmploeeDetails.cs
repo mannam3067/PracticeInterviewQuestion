e95a0830346fe12a34b5635362236f3052ae7e64
mallikharjuna.mannam <m.malli0005@gmail.com>
2021-06-13 14:15:29 +0530

    1.Added Employee Model class globaly

---
+++ Model/EmploeeDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeInterviewQuestions.Model
+{
+    public class EmploeeDetails
+    {        
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        //List<EmploeeDetails> emploeeDetails = new List<EmploeeDetails>
+        //{
+        //    new EmploeeDetails { ID = 101, Name = "mallikharjuna", Salary = 100000 },
+        //    new EmploeeDetails { ID = 102, Name = "Satya", Salary = 200000 },
+        //    new EmploeeDetails { ID = 103, Name = "sati", Salary = 300000 }
+        //};
+    }
+
+
+}
